d65543b3f8beb83397411857627d6e74fd467b7d
morwen80 <ribesnero80@gmail.com>
2019-03-01 11:06:01 +0000

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+
+const SimplerComponent = (props) =>
+
+// <div>{props.handleClick("happy")}</div>
+
+<div onClick={props.handleClick}>I am just happy</div>
+
+export default SimplerComponent;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+// const shoutMood = (mood) => {
+//  return `I am just ${mood}`
+// }
+
+const shout = () => {
+  alert("I AM SO SO SO happy")
+}
+
 ReactDOM.render(
+
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={shout} />
   </div>,
   document.getElementById('root')
 );
